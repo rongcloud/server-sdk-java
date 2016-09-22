201c47fcc7374c617da7952b0407a1786b042fee
师海阳 <shihaiyang@rongcloud.cn>
2016-09-22 16:31:00 +0800

    修改 gson 将 ==  转码bug

--- src/io/rong/util/GsonUtil.java
+++ src/io/rong/util/GsonUtil.java
@@ -4,22 +4,21 @@ import java.io.Reader;
 import java.lang.reflect.Type;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class GsonUtil {
 
-	private static Gson gson = new Gson();
-	
-	
+	private static Gson gson = new GsonBuilder().disableHtmlEscaping().create();
 
-	public static String toJson(Object obj, Type type) {		
-		return gson.toJson(obj, type);		
-	}		
-	
-	public static Object fromJson(String str,Type type){
+	public static String toJson(Object obj, Type type) {
+		return gson.toJson(obj, type);
+	}
+
+	public static Object fromJson(String str, Type type) {
 		return gson.fromJson(str, type);
 	}
-	
-	public static Object fromJson(Reader reader,Type type){
+
+	public static Object fromJson(Reader reader, Type type) {
 		return gson.fromJson(reader, type);
 	}
-}  
+}
