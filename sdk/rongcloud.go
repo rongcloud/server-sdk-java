83aa6c233ecd5a116981af683920760d11de1d7f
panxu <panxu@rongcloud.cn>
2019-01-04 16:34:23 +0800

    fix: 修复部分注释

--- sdk/rongcloud.go
+++ sdk/rongcloud.go
@@ -1,9 +1,30 @@
+// The MIT License (MIT)
+
+// Copyright (c) 2014 融云 Rong Cloud
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 /*
  * 融云 Server API go 客户端
  * create by RongCloud
  * create datetime : 2018-11-28
- *
- * v3.0.0
+ * v3
  */
 
 package sdk
@@ -21,9 +42,9 @@ import (
 
 const (
 	// RONGCLOUDSMSURI 容云默认 SMS API 地址
-	RONGCLOUDSMSURI = "http://172.29.202.3:18082"
+	RONGCLOUDSMSURI = "http://api.sms.ronghub.com"
 	// RONGCLOUDURI 容云默认 API 地址
-	RONGCLOUDURI = "http://172.29.202.3:18081"
+	RONGCLOUDURI = "http://api.cn.ronghub.com"
 	// ReqType body类型
 	ReqType = "json"
 	// USERAGENT sdk 名称
@@ -32,7 +53,7 @@ const (
 	DEFAULTTIMEOUT = 30
 )
 
-// RongCloud ak sk
+// RongCloud appKey appSecret extra
 type RongCloud struct {
 	appKey    string
 	appSecret string
