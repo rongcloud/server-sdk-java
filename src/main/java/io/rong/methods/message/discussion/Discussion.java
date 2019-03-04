748112895ee76baa4996fa62bf2b2bfa031af349
华超 <huachao@rongcloud.cn>
2019-03-04 19:51:34 +0800

    feat:消息发送接口 支持 contentAvailable 参数

--- src/main/java/io/rong/methods/message/discussion/Discussion.java
+++ src/main/java/io/rong/methods/message/discussion/Discussion.java
@@ -88,6 +88,9 @@ public class Discussion {
         if (message.getIsIncludeSender() != null) {
             sb.append("&isIncludeSender=").append(URLEncoder.encode(message.getIsIncludeSender().toString(), UTF8));
         }
+        if (message.getContentAvailable() != null) {
+            sb.append("&contentAvailable=").append(URLEncoder.encode(message.getContentAvailable().toString(), UTF8));
+        }
         String body = sb.toString();
         if (body.indexOf("&") == 0) {
             body = body.substring(1, body.length());
