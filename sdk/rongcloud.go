162b093738eea2193a82be413e2fcfc215a05213
SpringDRen <rlc0204@163.com>
2023-11-30 15:09:59 +0800

    发布 3.2.21

--- sdk/rongcloud.go
+++ sdk/rongcloud.go
@@ -63,7 +63,7 @@ const (
 	// ReqType body类型
 	ReqType = "json"
 	// USERAGENT sdk 名称
-	USERAGENT = "rc-go-sdk/3.2.20"
+	USERAGENT = "rc-go-sdk/3.2.21"
 	// DEFAULTTIMEOUT 默认超时时间，10秒
 	DEFAULTTIMEOUT = 10
 	// DEFAULT_KEEPALIVE http 默认保活时间，30秒
